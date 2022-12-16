fe5229dc5555e64fe651b63a722bb4e6d24e5738
Mikhael Skvortsov <mikhael.skvortsov@gmail.com>
2022-12-16 22:37:52 +0300

    Add scripts for incremental rebuild

---
+++ rebuild-init.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -ex
+
+docker build --target build-kit --tag build-kit ./node
---
+++ rebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -ex
+
+docker run --name build-kit --interactive --tty \
+--mount "type=bind,source=$(pwd)/node/ton-labs-executor,target=/ton-node/ton-labs-executor" \
+build-kit /bin/bash -c "/root/.cargo/bin/cargo update; /root/.cargo/bin/cargo build --release"
+
+docker cp build-kit:/ton-node/target/release/ton_node_startup ton-node
+docker rm build-kit
+
+docker rm --force evernode-se
+
+docker create --name evernode-se -e USER_AGREEMENT=yes -p80:80 tonlabs/evernode-se
+docker cp ton-node evernode-se:/node/
+rm ton-node
+
+docker start evernode-se
